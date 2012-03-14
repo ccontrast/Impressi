2beaffefdcf3ccee4423f3a98d403c371259f57b
Kimberly Lanning <kimberly.lanning@gmail.com>
2012-03-14 14:39:50 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impressi::Application.config.secret_token = '8d22fac0c235e68523de6d12f76248142ffde4e09cb1760bbcc2ee0a5f11221e27b05fd37ac51c8b7e10f9e8e20cae257d69da23a4071bce5aaf14bd434e879e'
